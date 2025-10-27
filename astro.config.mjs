e4ffffc610b0170b6d0766c978e03e4aae21cb29
Cyanez <cristianyanez2020@gmail.com>
2025-10-27 01:14:47 -0300

    config netlify
    config netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,18 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   server: {
     port: 4000,
   },
+
   integrations: [tailwind(), react()],
+
   devToolbar: {
     enabled: false,
   },
+
+  adapter: netlify(),
 });
